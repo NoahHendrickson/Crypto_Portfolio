ef1d321469c26bec349356f259b0f5bde68c8077
Noah Hendrickson <noahjames017@gmail.com>
2022-01-15 00:03:48 -0500

    starting over

---
+++ main.js
@@ -0,0 +1,9 @@
+const navLinks = document.querySelector('.nav-links');
+const dropdown = document.querySelector('.dropdown-menu')
+
+navLinks.addEventListener('click', e => {
+  if(e.target.matches('.dropdown-button')) {
+    dropdown.classList.toggle('active');
+  }
+  console.log('hello')
+})
