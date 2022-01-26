62772834b22ee14cb7c159a2811d87d329f99110
Noah Hendrickson <noahjames017@gmail.com>
2022-01-26 00:48:17 -0500

    restructured chart

--- main.js
+++ main.js
@@ -1,47 +1,4 @@
-var formatter = new Intl.NumberFormat("en-US", {
-  style: "currency",
-  currency: "USD",
-});
-
-fetch(
-  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-)
-  .then((response) => response.json())
-  .then((data) => cryptoInfo.push(data));
-
-const ctx = document.getElementById("myChart").getContext("2d");
-const myChart = new Chart(ctx, {
-  type: "doughnut",
-  options: {
-    legend: {
-      position: "bottom",
-    },
-  },
-  data: {
-    labels: [],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [],
-        backgroundColor: [
-          "rgba(2, 142, 119, .5)",
-          "rgba(250, 124, 8, .5)",
-          "rgba(145, 255, 156, .5)",
-          "rgba(244, 246, 231, .5)",
-          "rgba(132, 126, 115, .5)",
-        ],
-        borderColor: [
-          "rgba(2, 142, 119, 1)",
-          "rgba(250, 124, 8, 1)",
-          "rgba(145, 255, 156, 1)",
-          "rgba(244, 246, 231, 1)",
-          "rgba(132, 126, 115, 1)",
-        ],
-        borderWidth: 4,
-      },
-    ],
-  },
-});
+// Nav dropdown buttons
 
 const navLinks = document.querySelector(".nav-links");
 const doc = document;
@@ -67,73 +24,166 @@ document.addEventListener("click", (e) => {
   });
 });
 
+// slidedown buttons
+
 const main = document.querySelector(".main");
 
 main.addEventListener("click", (e) => {
   const slideDown = document.querySelector(".content-slide");
   const contentButton = document.querySelector(".content-button");
   // let checkmark = document.createTextNode("🆗");
-  const one = document.querySelector(".one");
   const five = document.querySelector(".five");
-  const six = document.querySelector(".six");
-  const seven = document.querySelector(".seven");
   if (e.target.matches(".content-button")) {
     e.target.classList.toggle("active");
-  }
-  if (e.target.matches(".four")) {
-    peakValue.classList.toggle("active");
   }
   if (e.target.matches(".five")) {
     currentValue.classList.toggle("active");
   }
-  if (e.target.matches(".six")) {
-    hourlyPL.classList.toggle("active");
-  }
-  if (e.target.matches(".seven")) {
-    monthlyPL.classList.toggle("active");
-  }
 });
-const hourlyPL = document.querySelector(".hourly-PL");
-const monthlyPL = document.querySelector(".monthly-PL");
-const currentValue = document.querySelector(".current");
-const peakValue = document.querySelector(".peak");
 
-const addCrypto = () => {
-  refreshPrices();
-  const table = document.querySelector(".wallet");
-  const row = table.insertRow();
-  const cell1 = row.insertCell();
-  const cell2 = row.insertCell();
-  const cell3 = row.insertCell();
-  const cell4 = row.insertCell();
-  const cell5 = row.insertCell();
+// USD format
 
-  let cryptoTicker = document.querySelector(".ticker").value;
-  cell1.innerHTML = cryptoTicker.toUpperCase();
-  for (i = 0; i < cryptoInfo[0].length; i++) {
-    if (cryptoInfo[0][i].symbol === cryptoTicker) {
-      cell4.innerHTML = cryptoInfo[0][i].current_price;
-    }
-  }
-  cell2.innerHTML = document.querySelector(".amount").value;
-  cell3.innerHTML = document.querySelector(".exchange").value;
-  const values = Number(cell2.innerHTML) * Number(cell4.innerHTML);
-  totalValues.push(values);
+var formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
 
-  cell5.innerHTML = formatter.format(values.toFixed(2));
-  assetNames.push(document.querySelector(".ticker").value);
-  assetAmounts.push(values);
-  const portfolioValue = totalValues.reduce(function (a, b) {
-    return a + b;
-  }, 0);
+// getting crypto info
 
-  currentValue.innerHTML = formatter.format(portfolioValue);
+fetch(
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+)
+  .then((response) => response.json())
+  .then((data) => cryptoInfo.push(data));
+
+// pie chart
+const options = {
+  legend: {
+    position: "bottom",
+  },
+  title: {
+    display: true,
+    text: "Portfolio",
+  },
 };
 
+const data = {
+  labels: [],
+
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [],
+      backgroundColor: [
+        "rgba(2, 142, 119, .5)",
+        "rgba(32, 176, 184, .5)",
+        "rgba(145, 255, 156, .5)",
+      ],
+      borderColor: [
+        "rgba(2, 142, 119, 1)",
+        "rgba(32, 176, 184, 1)",
+        "rgba(145, 255, 156, 1)",
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
+const ctx = document.getElementById("myChart").getContext("2d");
+
+const myChart = new Chart(ctx, {
+  type: "doughnut",
+  data: data,
+  options: options,
+});
+
+// const myChart = new Chart(ctx, {
+//   type: "pie",
+//   options: {
+//     plugins: {
+//       datalabels: {
+//         display: true,
+//         color: "red",
+//       },
+//     },
+//     legend: {
+//       position: "top",
+//     },
+//   },
+//   data: {
+//     labels: [],
+//     datasets: [
+//       {
+//         data: [],
+//         backgroundColor: [
+//           "rgba(2, 142, 119, .5)",
+//           "rgba(250, 124, 8, .5)",
+//           "rgba(145, 255, 156, .5)",
+//           "rgba(244, 246, 231, .5)",
+//           "rgba(132, 126, 115, .5)",
+//         ],
+//         borderColor: [
+//           "rgba(2, 142, 119, 1)",
+//           "rgba(250, 124, 8, 1)",
+//           "rgba(145, 255, 156, 1)",
+//           "rgba(244, 246, 231, 1)",
+//           "rgba(132, 126, 115, 1)",
+//         ],
+//         borderWidth: 3,
+//       },
+//     ],
+//   },
+// });
+
+const currentValue = document.querySelector(".current-value");
+
+const addCryptoToWallet = () => {
+  const table = document.querySelector(".wallet");
+  const row = table.insertRow();
+  const tickerCell = row.insertCell();
+  const amountCell = row.insertCell();
+  const exchangeCell = row.insertCell();
+  const priceCell = row.insertCell();
+  const totalCell = row.insertCell();
+
+  let cryptoTicker = document.querySelector(".ticker").value;
+  for (i = 0; i < cryptoInfo[0].length; i++) {
+    if (cryptoTicker === "avax") {
+      return;
+    }
+    if (cryptoInfo[0][i].symbol === cryptoTicker) {
+      tickerCell.innerHTML = cryptoTicker.toUpperCase();
+      priceCell.innerHTML = cryptoInfo[0][i].current_price;
+      amountCell.innerHTML = document.querySelector(".amount").value;
+      exchangeCell.innerHTML = document.querySelector(".exchange").value;
+
+      const values = Number(amountCell.innerHTML) * Number(priceCell.innerHTML);
+
+      totalValues.push(values);
+
+      totalCell.innerHTML = formatter.format(values.toFixed(2));
+      assetNames.push(document.querySelector(".ticker").value);
+      assetAmounts.push(values);
+      portfolioValues.push(
+        totalValues.reduce(function (a, b) {
+          return a + b;
+        }, 0)
+      );
+
+      console.log(portfolioValues);
+      // const percent = (Number(amountCell.innerHTML) * Number(priceCell.innerHTML)) / portfolioValue;
+      // cell6.innerHTML = percent.toFixed(2);
+      const portfolioValue = portfolioValues.pop();
+      currentValue.innerHTML = ` // ${formatter.format(portfolioValue)}`;
+    }
+  }
+};
+
+let portfolioValues = [];
 let totalValues = [];
 let assetNames = [];
 let assetAmounts = [];
 
+console.log(portfolioValues);
 function updateChart() {
   myChart.data.datasets[0].data = [];
   myChart.data.labels = [];
@@ -149,16 +199,7 @@ function updateChart() {
 
 let cryptoInfo = [];
 
-function refreshPrices() {
-  fetch(
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-  )
-    .then((response) => response.json())
-    .then((data) => cryptoInfo.push(data));
-}
-
 function addToPriceSlider() {
-  refreshPrices();
   const parentE = document.querySelector(".price-slider");
   const siblingE = document.querySelector(".asset-slideout");
   const newSpan = document.createElement("span");
@@ -174,12 +215,3 @@ function addToPriceSlider() {
     }
   }
 }
-
-// function getPrice() {
-//   let cryptoTicker = document.querySelector(".ticker").value;
-//   for (i = 0; i < cryptoInfo.length; i++) {
-//     if (cryptoInfo[i].symbol === cryptoTicker) {
-//       cell4.innerHTML = i.current_price
-//     }
-//   }
-// }
